598ed5ec428dbed6b540aa1ce5c28a7dbafd545b
Anselm Powell <anselm.powell@sport.wales>
2024-10-15 18:07:42 +0100

    migrate data

--- src/core/settings.py
+++ src/core/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = config("DJANGO_SECRET_KEY", cast=str)
 DEBUG = config("DJANGO_DEBUG", cast=bool, default=False)
 
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.railway.app']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://basedatastorev1-production.up.railway.app', 'basedatastorev1-production.up.railway.app']
 if DEBUG:
     ALLOWED_HOSTS = ["*"]
 
