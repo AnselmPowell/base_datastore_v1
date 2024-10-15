e9b7474476425331781e4e517435802944b39bf8
Anselm Powell <anselm.powell@sport.wales>
2024-10-15 17:26:18 +0100

    deployment config

--- src/core/settings.py
+++ src/core/settings.py
@@ -22,8 +22,15 @@ SECRET_KEY = config("DJANGO_SECRET_KEY", cast=str)
 DEBUG = config("DJANGO_DEBUG", cast=bool, default=False)
 
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.railway.app']
+if DEBUG:
+    ALLOWED_HOSTS = ["*"]
 
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://*.railway.app",
+    "https://*.railway.app",
+]
 
 
 CORS_ALLOWED_ORIGINS = []
